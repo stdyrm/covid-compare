2c0ef2586569728a83dd378fe7d40785d51ac772
stdyrm <17044124+stdyrm@users.noreply.github.com>
2020-05-15 22:22:34 -1000

    Code refactor

--- src/components/appGapminder/chart/ChartGapminder.js
+++ src/components/appGapminder/chart/ChartGapminder.js
@@ -33,51 +33,52 @@ export const ChartGapminder = props => {
     const { data, selector, wrapper, bounds } = props;
     const { xParam, yParam, cParam } = selector;
     const { wrapperWidth, wrapperHeight, margin } = wrapper;
-	const { width, height } = bounds;
+    const { width, height } = bounds;
 
     const { dataStates } = useContext(dataContext);
     const [scales, setScales] = useState(null);
-	const [dayCounter, setDayCounter] = useState(1);
-	const [opacityNotSel, setOpacityNotSel] = useState(0.6);
+    const [dayCounter, setDayCounter] = useState(1);
+    const [opacityNotSel, setOpacityNotSel] = useState(0.6);
 
-	const theme = useTheme();
-	const mqSmall = useMediaQuery(theme.breakpoints.down("sm"));
-	const useStyles = makeStyles(theme => ({
-		rootSVG: {
-			display: "inline-block",
-			position: "relative",
-			width: "100%",
-			verticalAlign: "middle",
-			overflow: "hidden",
-		},
-		chartWrapper: {
-			display: "inline-block",
-			position: "absolute",
-			top: 0,
-			left: 0,
-		},
-		bounds: {
-			width: width,
-			height: height,
-		},
-		axes: {
-			color: theme.palette.text.primary,
-		},
-		legend: {
-			fontWeight: 700,
-			fontSize: ".8rem",
-		},
-		sliderContainer: {
-			flexDirection: ()  => mqSmall ? "column" : "row",
-			justifyContent: () => mqSmall ? "center" : "space-between",
-			maxWidth: width,
-			margin: "0 auto",
-			alignItems: "center",
-		},
-		slider: {
-			width: () => mqSmall ? "95%" : "45%",
-		},
-	}));
+    const theme = useTheme();
+    const mqSmall = useMediaQuery(theme.breakpoints.down("sm"));
+    const useStyles = makeStyles(theme => ({
+        rootSVG: {
+            display: "inline-block",
+            position: "relative",
+            width: "100%",
+            verticalAlign: "middle",
+            overflow: "hidden",
+        },
+        chartWrapper: {
+            display: "inline-block",
+            position: "absolute",
+            top: 0,
+            left: 0,
+        },
+        bounds: {
+            width: width,
+            height: height,
+        },
+        axes: {
+            color: theme.palette.text.primary,
+        },
+        legend: {
+            color: theme.palette.primary.contrastText,
+            fontWeight: 700,
+            fontSize: ".8rem",
+        },
+        sliderContainer: {
+            flexDirection: () => (mqSmall ? "column" : "row"),
+            justifyContent: () => (mqSmall ? "center" : "space-between"),
+            maxWidth: width,
+            margin: "0 auto",
+            alignItems: "center",
+        },
+        slider: {
+            width: () => (mqSmall ? "95%" : "45%"),
+        },
+    }));
 
     const classes = useStyles();
 
@@ -127,7 +128,10 @@ export const ChartGapminder = props => {
         const xAxis = d3
             .select(xAxisRef.current)
             .call(xAxisGenerator)
-            .call(axis => axis.append("text").attr("class", "xLabel"));
+            .call(axis => {
+                axis.append("text").attr("class", "xLabel");
+                axis.append("text").attr("class", "counter-display");
+            });
 
         xAxis
             .selectAll(".xLabel")
@@ -135,9 +139,9 @@ export const ChartGapminder = props => {
             .join("text")
             .attr("x", width)
             .attr("y", 40)
-            .attr("class", "xLabel")
             .attr("text-anchor", "end")
-            .attr("fill", "white")
+            .attr("class", "xLabel")
+            .attr("fill", theme.palette.text.primary)
             .text(xParam.selected);
 
         const yAxis = d3
@@ -151,10 +155,10 @@ export const ChartGapminder = props => {
             .join("text")
             .attr("x", 0)
             .attr("y", -40)
-            .attr("class", "yLabel")
             .attr("text-anchor", "end")
             .attr("transform", "rotate(-90)")
-            .attr("fill", "white")
+            .attr("class", "yLabel")
+            .attr("fill", theme.palette.text.primary)
             .text(yParam.selected);
 
         // legend
@@ -203,66 +207,72 @@ export const ChartGapminder = props => {
         if (dataStates) {
             renderChart();
         }
-    }, [dataStates, selector]);
+    }, [dataStates, selector, theme]);
 
     useEffect(() => {
-		d3.select(".counter-display")
-			.join("text")
-			.text(`Day ${dayCounter}`);
-    }, [dayCounter]);
+        d3.select(boundsRef.current)
+            .selectAll(".counter-display")
+            .join("text")
+            .attr("text-anchor", "end")
+            .attr("x", width)
+            .attr("y", -40)
+            .attr("class", "counter-display")
+            .style("fill", theme.palette.text.primary)
+            .text(`Day ${dayCounter}`);
+    }, [dayCounter, theme]);
 
     return (
         <>
-			<div>			
-				<svg
-					id="gapminder-app"
-					height={wrapperHeight}
-					width={wrapperWidth}
-					className={classes.rootSVG}
-					viewBox={`0 0 ${wrapperWidth} ${wrapperHeight}`}
-				>
-					<g
-						ref={boundsRef}
-						className={classes.bounds}
-						transform={`translate(${margin.left}, ${margin.top})`}
-					>
-						<g
-							ref={xAxisRef}
-							transform={`translate(0, ${height})`}
-							className={classes.axes}
-						/>
-						<g ref={yAxisRef} className={classes.axes} />
-						<g
-							ref={legendRef}
-							transform={`translate(0, ${height})`}
-							className={classes.legend}
-						/>
-						{data && scales ? (
-							<CirclesGapminder
-								{...props}
-								scales={scales}
-								dayCounter={dayCounter}
-								opacityNotSel={opacityNotSel}
-							/>
-						) : (
-							<></>
-						)}
-					</g>
-				</svg>
-			</div>
+            <div>
+                <svg
+                    id="gapminder-app"
+                    height={wrapperHeight}
+                    width={wrapperWidth}
+                    className={classes.rootSVG}
+                    viewBox={`0 0 ${wrapperWidth} ${wrapperHeight}`}
+                >
+                    <g
+                        ref={boundsRef}
+                        className={classes.bounds}
+                        transform={`translate(${margin.left}, ${margin.top})`}
+                    >
+                        <g
+                            ref={xAxisRef}
+                            transform={`translate(0, ${height})`}
+                            className={classes.axes}
+                        />
+                        <g ref={yAxisRef} className={classes.axes} />
+                        <g
+                            ref={legendRef}
+                            transform={`translate(0, ${height})`}
+                            className={classes.legend}
+                        />
+                        {data && scales ? (
+                            <CirclesGapminder
+                                {...props}
+                                scales={scales}
+                                dayCounter={dayCounter}
+                                opacityNotSel={opacityNotSel}
+                            />
+                        ) : (
+                            <></>
+                        )}
+                    </g>
+                </svg>
+            </div>
             <Grid container className={classes.sliderContainer}>
                 <Grid item className={classes.slider}>
-					<DayCounterSlider 
-						dayCounter={dayCounter}
-						setDayCounter={setDayCounter}
-					/>
+                    <DayCounterSlider
+                        dayCounter={dayCounter}
+                        setDayCounter={setDayCounter}
+                    />
                 </Grid>
-				<Grid item className={classes.slider}>
-					<OpacitySlider 
-						opacityNotSel={opacityNotSel} 
-						setOpacityNotSel={setOpacityNotSel}
-					/>
-				</Grid>
+                <Grid item className={classes.slider}>
+                    <OpacitySlider
+                        opacityNotSel={opacityNotSel}
+                        setOpacityNotSel={setOpacityNotSel}
+                    />
+                </Grid>
             </Grid>
         </>
     );
