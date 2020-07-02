c521247edc191f422d3dc6643cb3b7c4aaa42354
stdyrm <17044124+stdyrm@users.noreply.github.com>
2020-07-01 15:55:27 -1000

    Updates

--- precache-manifest.fb9b0c31d70692336dbce06d62651b0c.js
+++ precache-manifest.fb9b0c31d70692336dbce06d62651b0c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "98148c95a3693d2a6d88cbc981659a9d",
+    "revision": "74ca2e213b4ff0b32e0c681f97976891",
     "url": "/covidcompare/index.html"
   },
   {
-    "revision": "f9d5bcfd367d0f3cbe3f",
+    "revision": "7688b2326c136df6d6fe",
     "url": "/covidcompare/static/css/main.21bcf1a4.chunk.css"
   },
   {
-    "revision": "011b157db4c5aefef6fe",
-    "url": "/covidcompare/static/js/2.4270ee0b.chunk.js"
+    "revision": "3d6bb05b3f193c97b190",
+    "url": "/covidcompare/static/js/2.3c70d2a7.chunk.js"
   },
   {
     "revision": "eac4cf64bf573541fd6dba29c78393a0",
-    "url": "/covidcompare/static/js/2.4270ee0b.chunk.js.LICENSE.txt"
+    "url": "/covidcompare/static/js/2.3c70d2a7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f9d5bcfd367d0f3cbe3f",
-    "url": "/covidcompare/static/js/main.82186df3.chunk.js"
+    "revision": "7688b2326c136df6d6fe",
+    "url": "/covidcompare/static/js/main.9b1431c9.chunk.js"
   },
   {
     "revision": "3b7759b90d7bde89c1fb",
@@ -40,7 +40,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covidcompare/static/media/raleway-medium-webfont.78c3ee5f.woff"
   },
   {
-    "revision": "7e3ab9af6d954917559df83c434267e9",
-    "url": "/covidcompare/static/media/us-states.7e3ab9af.csv"
+    "revision": "83d72fb9cb2d6313887cfbc99c293fd0",
+    "url": "/covidcompare/static/media/us-states.83d72fb9.csv"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covidcompare/precache-manifest.febdf1c6b5a806513fb21f8d2cdbac1b.js"
+  "/covidcompare/precache-manifest.fb9b0c31d70692336dbce06d62651b0c.js"
 );
 
 self.addEventListener('message', (event) => {
