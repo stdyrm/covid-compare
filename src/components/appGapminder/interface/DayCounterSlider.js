96f9124765e52e81f9c3354983b115cef7849ccc
stdyrm <17044124+stdyrm@users.noreply.github.com>
2020-10-10 12:07:44 -1000

    Updated through 10/9. Increased  gapminder limit to 220 days

--- src/components/appGapminder/interface/DayCounterSlider.js
+++ src/components/appGapminder/interface/DayCounterSlider.js
@@ -13,7 +13,7 @@ export const DayCounterSlider = props => {
         valueLabelDisplay="auto"
         step={1}
         min={1}
-        max={210}
+        max={220}
         onChange={(e, newVal) => setDayCounter(newVal)}
       />
     </>
